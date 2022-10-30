a11231cd8dd5d1c2403698e876f686a0a575ed75
kondo0602 <kondo8363@gmail.com>
2022-10-30 20:18:19 +0900

    feat: TwitterApiからの検索結果取得を実装

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TwitterController } from './controller/twitter.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env'] })],
+  controllers: [TwitterController],
+  providers: [],
 })
 export class AppModule {}
