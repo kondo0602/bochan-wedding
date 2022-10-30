3a6837c02ffc9a7497698544a689fdcdad28fa74
kondo0602 <kondo8363@gmail.com>
2022-10-30 20:18:37 +0900

    feat: TwitterApiからの検索結果表示を実装

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Pokemon from "components/Pokemon";
+import Twitter from "components/Twitter";
 
 export default function Home() {
   return (
     <div>
       <Pokemon />
+      <Twitter />
     </div>
   );
 }
