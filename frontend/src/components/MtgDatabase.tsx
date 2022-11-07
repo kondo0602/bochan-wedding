926ab4600644b0006dd675d2446de788a7816681
kondo0602 <kondo8363@gmail.com>
2022-11-07 19:28:17 +0900

    feat: 色を指定してのカード検索を実装

--- frontend/src/components/MtgDatabase.tsx
+++ frontend/src/components/MtgDatabase.tsx
@@ -6,13 +6,24 @@ import StyleIcon from "@mui/icons-material/Style";
 import { Card as MtgCard, CardFilter } from "../features/magics/types/index";
 import { useState } from "react";
 import axios from "axios";
-import { CardContent, CardMedia, Input, Stack } from "@mui/material";
+import {
+  CardContent,
+  CardMedia,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Input,
+  Radio,
+  RadioGroup,
+  Stack,
+} from "@mui/material";
 
 type Card = {};
 
 export const MtgDatabase = () => {
   const [cards, setCards] = useState<MtgCard[]>([]);
   const [searchWord, setSearchWord] = useState<string>("");
+  const [color, setColor] = useState<string>("");
 
   const handleChangeSearchWord = (
     event: React.ChangeEvent<HTMLInputElement>
@@ -21,11 +32,17 @@ export const MtgDatabase = () => {
     setSearchWord(input);
   };
 
+  const handleChangeColor = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const input = event.target.value;
+    setColor(input);
+  };
+
   const handleSubmit = () => {
     const endpoint = "https://api.magicthegathering.io/v1/cards";
 
     const params: CardFilter = {
       name: searchWord,
+      colors: color,
       // rarity: "rare",
       pageSize: 5,
     };
@@ -58,12 +75,33 @@ export const MtgDatabase = () => {
         Magic database
       </Typography>
       <CardContent sx={{ flexGrow: 1 }}>
-        <Input
-          fullWidth
-          value={searchWord}
-          placeholder="Please Enter TODO!"
-          onChange={handleChangeSearchWord}
-        />
+        <FormControl>
+          <Input
+            fullWidth
+            value={searchWord}
+            placeholder="Please Enter Card Name"
+            onChange={handleChangeSearchWord}
+          />
+          <FormLabel id="demo-radio-buttons-group-label">Color</FormLabel>
+          <RadioGroup
+            row
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue=""
+            name="radio-buttons-group"
+            onChange={handleChangeColor}
+          >
+            <FormControlLabel
+              value=""
+              control={<Radio />}
+              label="Unspecified"
+            />
+            <FormControlLabel value="W" control={<Radio />} label="White" />
+            <FormControlLabel value="U" control={<Radio />} label="Blue" />
+            <FormControlLabel value="B" control={<Radio />} label="Black" />
+            <FormControlLabel value="R" control={<Radio />} label="Red" />
+            <FormControlLabel value="G" control={<Radio />} label="Green" />
+          </RadioGroup>
+        </FormControl>
         {cards.length === 0 ? (
           <Typography gutterBottom variant="h5" component="h2">
             no result.
