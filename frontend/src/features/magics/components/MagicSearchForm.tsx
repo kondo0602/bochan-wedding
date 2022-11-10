acb059cc0e1c37dfbdb1f8bd127141eab388921f
kondo0602 <kondo8363@gmail.com>
2022-11-10 14:56:29 +0900

    style: 検索フォームの各項目間にスペースを追加

--- frontend/src/features/magics/components/MagicSearchForm.tsx
+++ frontend/src/features/magics/components/MagicSearchForm.tsx
@@ -42,118 +42,143 @@ export const MagicSearchForm = () => {
 
   return (
     <>
-      <FormControl>
-        <Input
-          fullWidth
-          value={searchWord}
-          placeholder="Please Enter Card Name"
-          onChange={handleChangeSearchWord}
-        />
-        <FormLabel id="demo-radio-buttons-group-label">Color</FormLabel>
-        <RadioGroup
-          row
-          aria-labelledby="demo-radio-buttons-group-label"
-          defaultValue=""
-          name="radio-buttons-group"
-          onChange={handleChangeColor}
-        >
-          <FormControlLabel value="" control={<Radio />} label="Unspecified" />
-          <FormControlLabel value="W" control={<Radio />} label="White" />
-          <FormControlLabel value="U" control={<Radio />} label="Blue" />
-          <FormControlLabel value="B" control={<Radio />} label="Black" />
-          <FormControlLabel value="R" control={<Radio />} label="Red" />
-          <FormControlLabel value="G" control={<Radio />} label="Green" />
-        </RadioGroup>
-
-        <FormLabel id="rarity">Rarity</FormLabel>
-        <RadioGroup
-          row
-          aria-labelledby="rarity"
-          defaultValue=""
-          name="radio-buttons-group"
-          onChange={handleChangeRarity}
-        >
-          <FormControlLabel value="" control={<Radio />} label="Unspecified" />
-          <FormControlLabel value="Common" control={<Radio />} label="Common" />
-          <FormControlLabel
-            value="Uncommon"
-            control={<Radio />}
-            label="Uncommon"
+      <Stack spacing={2}>
+        <FormControl>
+          <Input
+            fullWidth
+            value={searchWord}
+            placeholder="Please Enter Card Name"
+            onChange={handleChangeSearchWord}
           />
-          <FormControlLabel value="Rare" control={<Radio />} label="Rare" />
-          <FormControlLabel
-            value="Mythic Rare"
-            control={<Radio />}
-            label="Mythic Rare"
-          />
-        </RadioGroup>
-        <FormLabel id="type">Type</FormLabel>
-        <RadioGroup
-          row
-          aria-labelledby="type"
-          defaultValue=""
-          name="type"
-          onChange={handleChangeType}
-        >
-          <FormControlLabel value="" control={<Radio />} label="Unspecified" />
-          <FormControlLabel
-            value="Creature"
-            control={<Radio />}
-            label="Creature"
-          />
-          <FormControlLabel
-            value="Instant"
-            control={<Radio />}
-            label="Instant"
-          />
-          <FormControlLabel
-            value="Sorcery"
-            control={<Radio />}
-            label="Sorcery"
-          />
-          <FormControlLabel
-            value="Artifact"
-            control={<Radio />}
-            label="Artifact"
-          />
-          <FormControlLabel
-            value="Enchantment"
-            control={<Radio />}
-            label="Enchantment"
-          />
-          <FormControlLabel
-            value="Planeswalker"
-            control={<Radio />}
-            label="Planeswalker"
-          />
-          <FormControlLabel value="Land" control={<Radio />} label="Land" />
-        </RadioGroup>
-      </FormControl>
-      <Button variant="contained" onClick={handleSubmit}>
-        Fetch
-      </Button>
-      {cards.length === 0 ? (
-        <Typography gutterBottom variant="h5" component="h2">
-          no result.
-        </Typography>
-      ) : (
-        <Stack direction="row" spacing={2}>
-          {cards.map((card) => {
-            return (
-              <div key={card.id} onClick={() => handlePreview(card)}>
-                <CardMedia
-                  component="img"
-                  sx={{
-                    width: 150,
-                  }}
-                  image={card.imageUrl}
-                  alt="magic"
-                />
-              </div>
-            );
-          })}
+        </FormControl>
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">Color</FormLabel>
+          <RadioGroup
+            row
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue=""
+            name="radio-buttons-group"
+            onChange={handleChangeColor}
+          >
+            <FormControlLabel
+              value=""
+              control={<Radio />}
+              label="Unspecified"
+            />
+            <FormControlLabel value="W" control={<Radio />} label="White" />
+            <FormControlLabel value="U" control={<Radio />} label="Blue" />
+            <FormControlLabel value="B" control={<Radio />} label="Black" />
+            <FormControlLabel value="R" control={<Radio />} label="Red" />
+            <FormControlLabel value="G" control={<Radio />} label="Green" />
+          </RadioGroup>
+        </FormControl>
+        <FormControl>
+          <FormLabel id="rarity">Rarity</FormLabel>
+          <RadioGroup
+            row
+            aria-labelledby="rarity"
+            defaultValue=""
+            name="radio-buttons-group"
+            onChange={handleChangeRarity}
+          >
+            <FormControlLabel
+              value=""
+              control={<Radio />}
+              label="Unspecified"
+            />
+            <FormControlLabel
+              value="Common"
+              control={<Radio />}
+              label="Common"
+            />
+            <FormControlLabel
+              value="Uncommon"
+              control={<Radio />}
+              label="Uncommon"
+            />
+            <FormControlLabel value="Rare" control={<Radio />} label="Rare" />
+            <FormControlLabel
+              value="Mythic Rare"
+              control={<Radio />}
+              label="Mythic Rare"
+            />
+          </RadioGroup>
+        </FormControl>
+        <FormControl>
+          <FormLabel id="type">Type</FormLabel>
+          <RadioGroup
+            row
+            aria-labelledby="type"
+            defaultValue=""
+            name="type"
+            onChange={handleChangeType}
+          >
+            <FormControlLabel
+              value=""
+              control={<Radio />}
+              label="Unspecified"
+            />
+            <FormControlLabel
+              value="Creature"
+              control={<Radio />}
+              label="Creature"
+            />
+            <FormControlLabel
+              value="Instant"
+              control={<Radio />}
+              label="Instant"
+            />
+            <FormControlLabel
+              value="Sorcery"
+              control={<Radio />}
+              label="Sorcery"
+            />
+            <FormControlLabel
+              value="Artifact"
+              control={<Radio />}
+              label="Artifact"
+            />
+            <FormControlLabel
+              value="Enchantment"
+              control={<Radio />}
+              label="Enchantment"
+            />
+            <FormControlLabel
+              value="Planeswalker"
+              control={<Radio />}
+              label="Planeswalker"
+            />
+            <FormControlLabel value="Land" control={<Radio />} label="Land" />
+          </RadioGroup>
+        </FormControl>
+        <Stack justifyContent="flex-end">
+          <Button variant="contained" onClick={handleSubmit}>
+            Fetch
+          </Button>
         </Stack>
-      )}
+        {cards.length === 0 ? (
+          <Typography gutterBottom variant="h5" component="h2">
+            no result.
+          </Typography>
+        ) : (
+          <Stack direction="row" spacing={2}>
+            {cards.map((card) => {
+              return (
+                <div key={card.id} onClick={() => handlePreview(card)}>
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      width: 150,
+                    }}
+                    image={card.imageUrl}
+                    alt="magic"
+                  />
+                </div>
+              );
+            })}
+          </Stack>
+        )}
+      </Stack>
       <CardPreviewDialog
         open={isOpen}
         onClose={handleClose}
