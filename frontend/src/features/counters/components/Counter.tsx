3a2a8a03b803be0caac76919748de7951ad91047
kondo0602 <kondo8363@gmail.com>
2022-11-10 17:03:11 +0900

    style: レイアウトはコンポーネントの外側から指定するように修正

--- frontend/src/features/counters/components/Counter.tsx
+++ frontend/src/features/counters/components/Counter.tsx
@@ -1,70 +1,67 @@
-import * as React from "react";
+import AvTimerIcon from "@mui/icons-material/AvTimer";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import AvTimerIcon from "@mui/icons-material/AvTimer";
+import * as React from "react";
 import useCounter from "../hooks/useCounter";
 
 export const Counter = () => {
   const { count, error, increment, decrement, double } = useCounter();
 
   return (
-    <Grid item key={1} xs={12} sm={6} md={4}>
-      <Card
+    <Card
+      sx={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Typography
+        gutterBottom
+        variant="h5"
+        component="h2"
+        sx={{ pt: 2, pl: 2 }}
+      >
+        <AvTimerIcon sx={{ pt: 1, mr: 1 }} />
+        Counter
+      </Typography>
+      <Typography
+        gutterBottom
+        variant="h1"
+        component="h2"
+        mx="auto"
+        sx={{ pt: 2 }}
+      >
+        {count}
+      </Typography>
+      <CardContent sx={{ flexGrow: 1 }}>
+        {error && (
+          <Typography color="error">
+            Do not operate the number to become 4 digits as it will protrude
+            from the frame.
+          </Typography>
+        )}
+      </CardContent>
+      <CardActions
         sx={{
-          height: "100%",
+          pb: 2,
           display: "flex",
-          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <Typography
-          gutterBottom
-          variant="h5"
-          component="h2"
-          sx={{ pt: 2, pl: 2 }}
-        >
-          <AvTimerIcon sx={{ pt: 1, mr: 1 }} />
-          Counter
-        </Typography>
-        <Typography
-          gutterBottom
-          variant="h1"
-          component="h2"
-          mx="auto"
-          sx={{ pt: 2 }}
-        >
-          {count}
-        </Typography>
-        <CardContent sx={{ flexGrow: 1 }}>
-          {error && (
-            <Typography color="error">
-              Do not operate the number to become 4 digits as it will protrude
-              from the frame.
-            </Typography>
-          )}
-        </CardContent>
-        <CardActions
-          sx={{
-            pb: 2,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Button variant="contained" color="error" onClick={decrement}>
-            -1
-          </Button>
-          <Button variant="contained" color="primary" onClick={increment}>
-            +1
-          </Button>
-          <Button variant="contained" color="success" onClick={double}>
-            x2
-          </Button>
-        </CardActions>
-      </Card>
-    </Grid>
+        <Button variant="contained" color="error" onClick={decrement}>
+          -1
+        </Button>
+        <Button variant="contained" color="primary" onClick={increment}>
+          +1
+        </Button>
+        <Button variant="contained" color="success" onClick={double}>
+          x2
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
--- frontend/src/features/pokemons/components/Pokemon.tsx
+++ frontend/src/features/pokemons/components/Pokemon.tsx
@@ -1,12 +1,11 @@
-import { useEffect } from "react";
+import CatchingPokemonSharpIcon from "@mui/icons-material/CatchingPokemonSharp";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import CatchingPokemonSharpIcon from "@mui/icons-material/CatchingPokemonSharp";
-import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import { useEffect } from "react";
 import { useGetPokemons } from "../api/getPokemons";
 
 export function Pokemon() {
@@ -35,52 +34,50 @@ const PokemonPresenter = (props: pokemonPresenterProps) => {
   const { id, image, handleFetch } = props;
 
   return (
-    <Grid item key={id} xs={12} sm={6} md={4}>
-      <Card
+    <Card
+      sx={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Typography
+        gutterBottom
+        variant="h5"
+        component="h2"
+        sx={{ pt: 2, pl: 2 }}
+      >
+        <CatchingPokemonSharpIcon sx={{ pt: 1, mr: 1 }} />
+        Genger API
+      </Typography>
+      {/* 初代と2代目の画像が大きいので統一的に表示できるように修正する */}
+      <CardMedia
+        component="img"
         sx={{
-          height: "100%",
+          marginX: "auto",
+          width: 200,
+        }}
+        image={image}
+        alt="pokemon"
+      />
+      <CardContent sx={{ flexGrow: 1 }}>
+        <Typography gutterBottom>
+          On the night of a full moon, if shadows move on their own and laugh,
+          it must be Gengar’s doing.
+        </Typography>
+      </CardContent>
+      <CardActions
+        sx={{
+          pb: 2,
           display: "flex",
-          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <Typography
-          gutterBottom
-          variant="h5"
-          component="h2"
-          sx={{ pt: 2, pl: 2 }}
-        >
-          <CatchingPokemonSharpIcon sx={{ pt: 1, mr: 1 }} />
-          Genger API
-        </Typography>
-        {/* 初代と2代目の画像が大きいので統一的に表示できるように修正する */}
-        <CardMedia
-          component="img"
-          sx={{
-            marginX: "auto",
-            width: 200,
-          }}
-          image={image}
-          alt="pokemon"
-        />
-        <CardContent sx={{ flexGrow: 1 }}>
-          <Typography gutterBottom>
-            On the night of a full moon, if shadows move on their own and laugh,
-            it must be Gengar’s doing.
-          </Typography>
-        </CardContent>
-        <CardActions
-          sx={{
-            pb: 2,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Button variant="contained" onClick={handleFetch}>
-            Fetch
-          </Button>
-        </CardActions>
-      </Card>
-    </Grid>
+        <Button variant="contained" onClick={handleFetch}>
+          Fetch
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
