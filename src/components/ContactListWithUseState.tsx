d0909d2194d6de7586fcea8bffc3a02cd1632451
kondo0602 <kondo8363@gmail.com>
2022-11-19 18:51:36 +0900

    feat: 連絡先の追加・削除を実装

--- src/components/ContactListWithUseState.tsx
+++ src/components/ContactListWithUseState.tsx
@@ -7,9 +7,11 @@ import {
   List,
   ListItem,
 } from "@mui/material";
-import { useState } from "react";
+import React, { useState } from "react";
+import createRandomString from "@/utils/createRandomString";
 
 type ContactAddress = {
+  id: string;
   email: string;
   mode: "view" | "edit";
 };
@@ -17,14 +19,45 @@ type ContactAddress = {
 export const ContactListWithUseState = () => {
   const [contactList, setContactList] = useState<ContactAddress[]>([
     {
+      id: "1",
       email: "kondo8363@gmail.com",
       mode: "edit",
     },
     {
+      id: "2",
       email: "kondo7778363@gmail.com",
       mode: "view",
     },
   ]);
+
+  const [input, setInput] = useState<string>("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const input = event.target.value;
+    setInput(input);
+  };
+  const handleAdd = () => {
+    setContactList([
+      ...contactList,
+      { id: createRandomString(), email: input, mode: "view" },
+    ]);
+    setInput("");
+  };
+
+  // const handleToggleMode = (id: string) => {
+  //   const newContactList = contactList.map((contact) => {
+  //     if (contact.id === id) {
+  //       return { ...contact, mode: contact.mode === "view" ? "edit" : "view" };
+  //     }
+  //     return contact;
+  //   });
+  // TODO: ここでUNION型にstringを割り当てることができないというエラーになる
+  //   setContactList(newContactList);
+  // };
+
+  const handleRemove = (id: string) => {
+    setContactList(contactList.filter((contact) => contact.id !== id));
+  };
 
   return (
     <Card
@@ -41,28 +74,47 @@ export const ContactListWithUseState = () => {
         <List>
           {contactList.map((item) => (
             <ListItem key={item.email}>
-              <ContactListItem {...item} />
+              <ContactListItem
+                {...item}
+                input={input}
+                handleDelete={handleRemove}
+              />
             </ListItem>
           ))}
         </List>
-        <Button variant="contained">Add</Button>
+        <Input
+          value={input}
+          placeholder="Please Enter Email Address"
+          onChange={handleChange}
+        />
+        <Button variant="contained" onClick={handleAdd}>
+          Add
+        </Button>
       </CardContent>
     </Card>
   );
 };
 
-export const ContactListItem = (props: ContactAddress) => {
+type ContactListItemProps = {
+  id: string;
+  email: string;
+  mode: "view" | "edit";
+  input: string;
+  handleDelete: (id: string) => void;
+};
+
+export const ContactListItem = (props: ContactListItemProps) => {
   return (
     <>
       {props.mode === "view" ? (
         <Typography variant="body2" color="text.secondary">
           {props.email}
           <Button>edit</Button>
-          <Button>delete</Button>
+          <Button onClick={() => props.handleDelete(props.id)}>delete</Button>
         </Typography>
       ) : (
         <>
-          <Input placeholder="Please Enter Email Adress"></Input>
+          <Input value={props.email}></Input>
           <Button>save</Button>
         </>
       )}
