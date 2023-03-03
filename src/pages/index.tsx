16e51631e35adacb1e2ad19d2232e780ddd8a172
kondo0602 <kondo8363@gmail.com>
2023-03-03 22:28:14 +0900

    feat:Bingoコンポーネントを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,83 @@
-export default function Bingo() {
-  return <div>ここに作っていきます</div>;
-}
+import { HalfScreen } from "@/components/HalfScreen";
+import { Box, Button, Grid, Stack, Typography } from "@mui/material";
+import { useState } from "react";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
+
+const Bingo = () => {
+  const handle = useFullScreenHandle();
+
+  type Number = {
+    number: number;
+    isHit: boolean;
+  };
+
+  const getInitialNumbers = (): Number[] => {
+    const numbers: Number[] = [];
+    for (let i = 0; i < 75; i++) {
+      numbers.push({ number: i + 1, isHit: false });
+    }
+    return numbers;
+  };
+
+  const [numbers, setNumbers] = useState<Number[]>(getInitialNumbers());
+  const [pickedNumber, setPickedNumber] = useState<number>(0);
+
+  const pickUnhitNumber = (): void => {
+    const unhitNumbers = numbers.filter((number) => !number.isHit);
+    const pickedNumber =
+      unhitNumbers[Math.floor(Math.random() * unhitNumbers.length)];
+    pickedNumber.isHit = true;
+    setNumbers([...numbers]);
+    setPickedNumber(pickedNumber.number);
+  };
+
+  return (
+    <>
+      <button onClick={handle.enter}>Enter fullscreen</button>
+
+      <FullScreen handle={handle}>
+        {handle.active && (
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              height: "100vh",
+              background: "white",
+            }}
+          >
+            <HalfScreen>
+              <Stack spacing={4}>
+                <Typography variant="h1">
+                  {pickedNumber !== 0 ? pickedNumber : "?"}
+                </Typography>
+                <Button variant="contained" onClick={pickUnhitNumber}>
+                  start
+                </Button>
+              </Stack>
+            </HalfScreen>
+            <HalfScreen>
+              <Typography>これまでに出た数字は...</Typography>
+              {/* TODO: 15ごとに縦に並んだ方が見やすい */}
+              <Grid container columns={5} spacing={1}>
+                {numbers.map((number) => (
+                  <Grid
+                    component="div"
+                    item
+                    xs={1}
+                    sx={{ display: "flex", justifyContent: "center" }}
+                  >
+                    <Typography color={number.isHit ? "black" : "lightGray"}>
+                      {number.number}
+                    </Typography>
+                  </Grid>
+                ))}
+              </Grid>
+            </HalfScreen>
+          </Box>
+        )}
+      </FullScreen>
+    </>
+  );
+};
+
+export default Bingo;
